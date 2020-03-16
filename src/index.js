b40d0fc49acfb8b8c3d48b0116ca00b16c12619a
mayinmarch <marfa.astafieva@gmail.com>
2020-03-17 00:09:29 +0300

    feat: implement typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  let minimum = array[0];
+  array.forEach(element => {
+    if(minimum > element){
+      minimum = element;
+    }
+  });
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  let maximum = array[0];
+  array.forEach(element => {
+    if (maximum < element){
+      maximum = element;
+    }
+  });
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0;
+  }
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum / array.length;
 }
